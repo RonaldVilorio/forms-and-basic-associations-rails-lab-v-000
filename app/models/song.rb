2b8a31535785140783009e8238453bdf5bb45fb9
ronald vilorio <soccer23415@gmail.com>
2018-04-05 19:28:43 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,10 +7,11 @@ class Song < ActiveRecord::Base
     self.artist = Artist.find_or_create_by(name: name)
   end
   def artist_name
-    self.artist.name
+    self.artist
   end
+
   def genre_name=(name)
-    self.genre = Genre.find_or_create_by(name: id)
+    self.genre = Genre.find_by(name: id)
   end
   def genre_name
     self.genre
